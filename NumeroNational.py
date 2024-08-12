5374f9fd07a72488bb99063848844d74d5318707
Tenetomb <109283597+Tenetomb@users.noreply.github.com>
2024-08-12 17:05:48 +0200

    Add files via upload

---
+++ NumeroNational.py
@@ -0,0 +1,16 @@
+#670113-456-56
+#670!13-456-56
+#
+
+list_n_n = ["17073èè0033 84", "85 07 30 033 28"]
+
+
+import re
+
+def remplacer_caracteres_speciaux(texte):
+    return re.sub(r'[^a-zA-Z0-9\s]', ' ', texte)
+
+
+for i in range(len(list_n_n[]))
+remplacer_caracteres_speciaux(list_n_n)
+print(list_n_n[0])
